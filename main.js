493a27bd2a4d1f9267dd9fa26bbefd1e7624ef5f
VariXx <VariXx@gmail.com>
2020-09-04 00:02:58 -0500

    youtube-dl test

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log(`goteem`);
+
+// https://github.com/przemyslawpluta/node-youtube-dl/issues/327 
+
+const youtubedl = require('youtube-dl');
+const fs = require('fs');
+
+const video = youtubedl(`https://www.twitch.tv/videos/697523995`);
+video.on('info', (info) => {
+    console.log('info');
+    console.log(info);
+});
+video.pipe(fs.createWriteStream('output.mkv'));
+video.on('complete', (info) => {
+    console.log('complete');
+    console.log(info)
+});
+video.on('end', () => {
+    console.log('end');
+});
+
